418876799db7dbca3999e885041a15830338c729
Jecho Carlos <jericho.carlos@cambridge.org>
2023-07-14 00:08:16 +0800

    feat(bot): presence update + small restructure

--- index.ts
+++ index.ts
@@ -1,28 +1,47 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { ActivitiesOptions, ActivityType, Client, GatewayIntentBits } from 'discord.js';
 import { config } from "dotenv";
-import { registerCommands } from './src/config/commandsRegister';
 import { handleInteraction } from './src/events/interactionCreate';
 import { handleMessageCreate } from './src/events/messageCreate';
+import { registerCommands } from './src/startup/commandsRegister';
 import { loadDb } from './src/startup/dbLoader';
 
 config();
+index();
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.DirectMessages
-	]
-});
+export function index() {
+	const client = new Client({
+		intents: [
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.DirectMessages
+		]
+	});
 
-client.once('ready', () => {
-	registerCommands(
-		process.env.CLIENT_ID,
-		process.env.DISCORD_BOT_TOKEN
-	);
-	loadDb();
-	console.log('Bot is ready!');
-});
+	client.once('ready', () => {
+		loadDb();
+		registerCommands(
+			process.env.CLIENT_ID,
+			process.env.DISCORD_BOT_TOKEN
+		);
+		updatePresence(client);
+		console.log('Bot is ready!');
+	});
 
-client.on('messageCreate', handleMessageCreate);
-client.on('interactionCreate', handleInteraction);
-client.login(process.env.DISCORD_BOT_TOKEN);
+	client.on('messageCreate', handleMessageCreate);
+	client.on('interactionCreate', handleInteraction);
+	client.login(process.env.DISCORD_BOT_TOKEN);
+}
+
+function updatePresence(client: Client) {
+	if (!(client.user)) return;
+
+	const activities: ActivitiesOptions[] = [{
+		type: ActivityType.Playing,
+		url: "https://github.com/cryzereye/keebot-refactor",
+		name: "ping @gego for bugs"
+	}];
+
+	client.user.setPresence({
+		activities: activities,
+		status: "online"
+	});
+}
